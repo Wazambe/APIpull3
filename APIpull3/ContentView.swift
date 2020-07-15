519f71e0965a3a85ad1b2daabe318a47f47192b1
Mark Robberts <robberts75@gmail.com>
2020-07-15 20:16:23 +0200

    Initial Commit

---
+++ APIpull3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  APIpull3
+//
+//  Created by Mark Robberts on 2020/07/15.
+//  Copyright © 2020 Mark Robberts. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
